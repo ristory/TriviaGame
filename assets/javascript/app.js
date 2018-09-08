35b489531c3fe7cb7695c0bb350c2d066153c0cc
hoang <hoangcao@mail.weber.edu>
2018-09-08 09:58:09 -0600

    finish

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,38 @@
 
+var answer = ["Faithful","Intelligent","Playful","Tusks","Biodegradable","Atmosphere","Solar energy","Oil tankers","Deforestation","Habitat"]
+var challenge = {
+  question1:["If a married man refrains from having affairs with other women we can say that he is","Loyal","Endearing","Faithful","Reassuring"],
+
+  question2:["What is the most suitable word to describe someone who is a deep and systematic thinker","Clever","Smart","Intelligent","Dull"],
+
+  question3:["Animals which like to play can be said to be","Play-liking","Play-centered","Playful","Players"],
+
+  question4:["Elephants are able to kill and injure other animals by making use of sharp and pointed things called","Long teeth","Tusks","Trunks","Hooves"],
+
+  question5:["Substances which dissolve into the ground after being thrown away are said to be","Environmentally friendly","Biodegradable","Untraceable","Dis-solvent"],
+
+  question6:["The layer of air surrounding the earth, and which is a source of oxygen, is called","The outer layer","Stratosphere","Ozone layer","Atmosphere"],
+
+  question7:["When we make use of the sun to produce power, we call this type of power","Solar energy","Sun energy","Light energy","Solar force"],
+
+  question8:["Ships used to transport oil are called","Oil ships","Oil tanks","Oil tankers","Oil boats"],
+
+  question9:["What word or words describe the gradual decline in the amount of land occupied by forests","Forest shrinking","Forest decline","Forest declining","Deforestation"],
+
+  question10:["The area of land that animals occupy, and which they depend on as a source of food, is called","Habitation","Habitat","Arable","Land territory"],
+}
+
+var array = [challenge.question1, challenge.question2,challenge.question3,challenge.question4,challenge.question5,challenge.question6,challenge.question7,
+  challenge.question8,challenge.question9,challenge.question10];
 var rightanswer = 0;
 var wrong = 0;
 var noanswer = 0;
 var firstclick = false;
 var firsttrue = true;
 var finish = false;
+var random;
+var number;
+var randomnumber;
 var end = 0;
 var a;
 var b;
@@ -12,13 +40,32 @@ var c;
 var d;
 
 $(".content").hide();
+$(".reset").hide();
 window.onload = function () {
     $(".startgame").on("click",function()
     {
       startquestion();
       stopwatch.start();
       $(".btn").hide();
-      $(".content").show();       
+      $(".content").show(); 
+
+    });
+
+    $(".reset").on("click",function()
+    {
+      end = 0;
+      rightanswer = 0;
+      wrong = 0;
+      noanswer = 0;
+      firstclick = false;
+      firsttrue = true;
+      finish = false;
+      array = [challenge.question1, challenge.question2,challenge.question3,challenge.question4,challenge.question5,challenge.question6,challenge.question7,
+        challenge.question8,challenge.question9,challenge.question10];
+      
+      nextquestion();
+      $(".reset").hide();
+      $(".content").show();
     });
 
     $(".answer1").on("click",function()
@@ -39,7 +86,6 @@ window.onload = function () {
             else if(firsttrue === true && element !=a )
             {
               $("#response").text("You got a wrong answer!!!");
-              $("#result").text("You have " + wrong + " answer");
               stopwatch.stop();
               stopwatch.reset();      
             }
@@ -138,36 +184,11 @@ window.onload = function () {
 
   }
 
-var answer = ["Faithful","Intelligent","Playful","Tusks","Biodegradable","Atmosphere","Solar energy","Oil tankers","Deforestation","Habitat"]
-var challenge = {
-  question1:["If a married man refrains from having affairs with other women we can say that he is","Loyal","Endearing","Faithful","Reassuring"],
-
-  question2:["What is the most suitable word to describe someone who is a deep and systematic thinker","Clever","Smart","Intelligent","Dull"],
-
-  question3:["Animals which like to play can be said to be","Play-liking","Play-centered","Playful","Players"],
-
-  question4:["Elephants are able to kill and injure other animals by making use of sharp and pointed things called","Long teeth","Tusks","Trunks","Hooves"],
-
-  question5:["Substances which dissolve into the ground after being thrown away are said to be","Environmentally friendly","Biodegradable","Untraceable","Dis-solvent"],
-
-  question6:["The layer of air surrounding the earth, and which is a source of oxygen, is called","The outer layer","Stratosphere","Ozone layer","Atmosphere"],
-
-  question7:["When we make use of the sun to produce power, we call this type of power","Solar energy","Sun energy","Light energy","Solar force"],
-
-  question8:["Ships used to transport oil are called","Oil ships","Oil tanks","Oil tankers","Oil boats"],
-
-  question9:["What word or words describe the gradual decline in the amount of land occupied by forests","Forest shrinking","Forest decline","Forest declining","Deforestation"],
-
-  question10:["The area of land that animals occupy, and which they depend on as a source of food, is called","Habitation","Habitat","Arable","Land territory"],
-}
-
-var array = [challenge.question1, challenge.question2,challenge.question3,challenge.question4,challenge.question5,challenge.question6,challenge.question7,
-  challenge.question8,challenge.question9,challenge.question10];
 function startquestion()
 { 
-    var random = array[Math.floor(Math.random()*10)];
-    var number = (array.splice(array.indexOf(random), 1));
-    var randomnumber =  number[0]; 
+    random = array[Math.floor(Math.random()*10)];
+    number = (array.splice(array.indexOf(random), 1));
+    randomnumber =  number[0]; 
     $("#question").text(randomnumber[0]); 
     a = randomnumber[1];
     b = randomnumber[2];
@@ -191,6 +212,10 @@ function nextquestion()
   stopwatch.start();
   if( end === 10)
   {
+    $(".content").hide();
+    stopwatch.stop();
+    $(".reset").show();
+    $(".reset").text("Start Over!!!!");
     endfunc();
   }
   else
@@ -210,8 +235,6 @@ function endfunc()
     `
     );
 }
-
-
 
 var intervalId  
 var clockRunning = false
@@ -248,6 +271,7 @@ var stopwatch = {
         noanswer++;
         stopwatch.stop();
         stopwatch.reset();
+        nextquestion();
        }
   }
 }
